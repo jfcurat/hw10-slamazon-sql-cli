ed5872d64aef326317ee1ec1cdf975aa0c64d8a0
James Curatalo <jfcurat@gmail.com>
2018-02-25 06:22:06 -0800

    Clean-Up
    got rid of commented wip code
    got rid of some other old comments and console.logs
    added $ to prices column display
    tried to make code formatting better/more readable - hopefully

--- slamazonCustomer.js
+++ slamazonCustomer.js
@@ -15,8 +15,8 @@ const inquirer = require('inquirer');
 const mysql = require('mysql');
 const Table = require('cli-table2');
 
-const USER = 'james'; // create tester later to post?
-const PASSWORD = 'james666'; // create tester later to post?
+const USER = 'james';
+const PASSWORD = 'james666';
 const DATABASE = 'slamazon';
 
 const connection = mysql.createConnection({
@@ -30,20 +30,23 @@ const connection = mysql.createConnection({
 
 connection.connect(function (error) {
   if (error) throw error;
+
   console.log(`Connected as id ${connection.threadId}`);
+
   getItemsList();
+
   inquisition();
 
   connection.end();
 });
 
-// ask for ID & amount to buy
+// ask for item_id & amount to buy
 function inquisition() {
   inquirer
     .prompt([
       {
         type: 'input',
-        message: 'Please enter ID of product you\'d like to buy',
+        message: 'Please enter ID of product you\'d like to buy:',
         name: 'productToBuy',
       },
       {
@@ -63,34 +66,24 @@ function getItemsList() {
   const inventoryColumns = ['item_id', 'product_name', 'department_name', 'price'];
   const query = connection.query('SELECT ?? FROM ??', [inventoryColumns, 'products'], function (error, results, fields) {
     if (error) throw error;
-    console.log(`\nInventory: `);// \nitem id | name | department | price`)
-    // for (var i = 0; i < results.length; i++) {
-    //   console.log(`${results[i].item_id} | ${results[i].product_name} | ${results[i].department_name} | \$${parseFloat(results[i].price).toFixed(2)}`);
-    // }
+    console.log(`\nInventory: `);
+
     const inventoryTable = new Table({
       head: inventoryColumns,
       colWidths: [10, 45, 25, 10]
     });
+
     for (let i = 0; i < results.length; i++) {
       inventoryTable.push(
-        [results[i].item_id, results[i].product_name, results[i].department_name, parseFloat(results[i].price).toFixed(2)],
-        // ['First value', 'Second value', '3rd', '4th']
+        [
+          results[i].item_id,
+          results[i].product_name,
+          results[i].department_name,
+          '\$' + parseFloat(results[i].price).toFixed(2)
+        ]
       );
     }
     console.log(inventoryTable.toString());
   });
   console.log(query.sql);
-
-  // const query0 = connection.query('SELECT item_id, product_name, price FROM products', function (err, res) {
-  //   if (err) throw err;
-  //   for (var i = 0; i < res.length; i++) {
-  //     console.log(`${res[i].item_id} | ${res[i].product_name} | ${res[i].price}`);
-  //   }
-  //   // display inventory table w/ cli-table2 later
-  //   const availableStockTable = new Table({
-  //     head: ['item id', 'name', 'price'],
-  //     colWidths: [100, 100, 100]
-  //   });
-  // });
-  // console.log(query0.sql);
 }
