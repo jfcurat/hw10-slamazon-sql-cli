9590fb9218c75a7175ee3eeb2b1c8e942469b45e
James Curatalo <jfcurat@gmail.com>
2018-02-27 05:09:23 -0800

    Clean-Up
    cleaned out extra console.log lines and comments.

--- slamazonCustomer0.js
+++ slamazonCustomer0.js
@@ -1,5 +1,4 @@
 // reroll attempt on customer app
-
 const inquirer = require('inquirer');
 const mysql = require('mysql');
 const Table = require('cli-table2');
@@ -20,21 +19,18 @@ const connection = mysql.createConnection({
 
 connection.connect(err => {
   if (err) throw err;
-  console.log(`Connected as id ${connection.threadId}`);
 
-  makeInventoryTable()//.then(makeOrder()).catch(err => {console.error(err)});
-  //makeOrder();
-  //connection.end();
+  makeInventoryTable();
 });
 
 function makeInventoryTable() {
   let inventoryColumns = ['item_id', 'product_name', 'department_name', 'price', 'stock_quantity'];
-
+  
   const query = connection.query('SELECT ?? FROM ??',
     [inventoryColumns, 'products'],
     (err, res) => {
       if (err) throw err;
-
+      
       console.log(`\nInventory: `);
 
       let inventoryTable = new Table({
@@ -58,15 +54,6 @@ function makeInventoryTable() {
       makeOrder();
     }
   );
-  console.log(query.sql);
-  /* 
-    return new Promise((resolve, reject) => {
-      if (reject) {
-        console.log(`promise rejected`);
-      }
-      console.log(resolve); // ??
-    }); */
-  //makeOrder();
 }
 
 function makeOrder() {
@@ -82,12 +69,8 @@ function makeOrder() {
       name: 'amountToBuy',
     }
   ]).then(answers => {
-    console.log(`\nfrom makeOrder, productToBuy: ${answers.productToBuy}, amountToBuy: ${answers.amountToBuy}`);
-
     var productToBuy = Number(answers.productToBuy);
     var amountToBuy = Number(answers.amountToBuy);
-    console.log(`\npass from makeOrder to checkStock, productToBuy = ${productToBuy}`);
-    console.log(`pass from makeOrder to checkStock, amountToBuy = ${amountToBuy}`);
 
     checkStock(productToBuy, amountToBuy);
   }).catch(err => {
@@ -96,8 +79,6 @@ function makeOrder() {
 }
 
 function checkStock(productToBuy, amountToBuy) {
-  console.log(`\nfrom checkStock, did it work? Buy itemID ${productToBuy}. how many? ${amountToBuy}`);
-
   let stockCheckColumns = ['item_id', 'price', 'stock_quantity'];
 
   const queryStockCheck = connection.query(
@@ -105,17 +86,10 @@ function checkStock(productToBuy, amountToBuy) {
     [stockCheckColumns, 'products', { item_id: productToBuy }],
     (err, res) => {
       if (err) throw err;
-      console.log(`\ncheckStock action worked?`);
-      console.log(res);
-      console.log(productToBuy);
 
       var stockAvailable = res[0].stock_quantity;
       var unitPrice = res[0].price;
-      console.log(`\nunits in stock: ${stockAvailable}`);
-      console.log(`amountToBuy = ${amountToBuy}`);
-      console.log(`price per unit = ${unitPrice}`);
       var stockRemaining = stockAvailable - amountToBuy;
-      console.log(`remaining stock: ${stockRemaining}`);
 
       if (stockRemaining >= 0) {
         console.log(`\nThe item is in stock.`);
@@ -129,23 +103,16 @@ function checkStock(productToBuy, amountToBuy) {
       }
     }
   );
-  console.log(`\nquery from checkStock: ${queryStockCheck.sql}`);
 }
 
 function updateStock(productToBuy, amountToBuy, unitPrice, stockRemaining) {
-  console.log(`\nstock update time...`);
-  console.log(`from updateStock, amountToBuy = ${amountToBuy}`);
-  console.log(`from updateStock, unitPrice = ${unitPrice}`);
-  console.log(`from updateStock, stockRemaining = ${stockRemaining}`);
   var totalCost = amountToBuy * unitPrice;
-  console.log(`from updateStock, totalCost = ${totalCost}`);
 
   const stockUpdateQuery = connection.query(
     'UPDATE ?? SET ? WHERE ?',
     ['products', { stock_quantity: stockRemaining }, {item_id: productToBuy}],
     (err, res) => {
       if (err) throw err;
-      console.log(res);
       console.log(`\nYour total cost is \$${totalCost}`);
 
       setTimeout(() => console.log(`\nThank you! Come again!`), 1666);
@@ -153,5 +120,4 @@ function updateStock(productToBuy, amountToBuy, unitPrice, stockRemaining) {
       setTimeout(() => connection.end(), 2666);
     }
   );
-  console.log(`\nquery from updateStock: ${stockUpdateQuery.sql}`);
 }
